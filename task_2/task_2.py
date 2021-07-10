f12f7decd3dae1522a1ee3e3704d4316f831bcb0
johnk2280 <johnk2280@gmail.com>
2021-07-10 20:39:20 +0500

    [add] added task_2

---
+++ task_2/task_2.py
@@ -0,0 +1,24 @@
+6.1
+TDS_PATH - TRAIN_DATASET_PATH // путь к тренировочному датасету
+c_max - max_number_in_column // максимальное числов в столбце
+c_min - min_number_in_column // минимальное число в столбце
+df - data_frame // набор данных
+train_df - train_data_frame // тренировочный набор данных
+
+6.2
+eratosthenes_sieve // решето Эратосфена
+bubble_sort // сортировка пузырьком
+merge_sort // сортировка слиянием
+breadth_first_search // поиск в ширину
+
+6.3
+class Catalog: [products, get_goods_quantity, add_product_to_basket]
+class Basket: [get_total_basket_price, render_product_row, render_total_price_row]
+class AddOrdersForm: [add_order, _create_table, get_order_data, _add_row, _delete_row]
+
+6.4
+temp - temp_list // промежуточный список
+url - catigories_url // url по которому возвращяется список категорий
+num_row - number_of_row // номер строки
+col_num - nember_of_column // номер столбца
+del_app - delete_order // удалить заявку
