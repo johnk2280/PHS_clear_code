4b11762e0ecf3c6250e74192df414385270ae41f
johnk2280 <johnk2280@gmail.com>
2021-07-18 19:34:03 +0500

    [add] added task_4 solution

---
+++ task_4/task_4.py
@@ -0,0 +1,13 @@
+create - insert_row  # наполнение файла построчно
+pointer - line_num  # номер текущей строки в редакторе
+list_of_lists - two_d_array  # двумерная матрица
+cur_str - current_edit_row  # текущая строка в редакторе
+workers_list - workers  # список всех сотрудников
+rus_dict - en_to_rus_dict  # словарь: ключи - латинские слова, значения - русские аналоги латинских слов
+time_red - green_light_time  # время горения зеленого цвета светофора
+numbers_list - numbers  # список чисел
+decimal_value - transfered_number  # результат конвертации из одной системы счисления в другую
+total_votes - votes_sum  # общее количество голосов
+vote_result - election_result  # результат голосования на выборах
+sum_res - balance  # итог бухгалтерской сводки
+
