1df817b31da10317525f0fec317630f90cd4706a
johnk2280 <johnk2280@gmail.com>
2021-07-26 19:56:54 +0500

    [add] added task_6

---
+++ task_6/task_6.py
@@ -0,0 +1,6 @@
+add_data - upload_file_data  # загрузи данные из файла
+read - get_file_data  # получи данные из файла
+read_context_file - get_file_data  # получи данные из файла
+get_data - get_page_data  # получи данные страницы
+check_file_upload - get_upload_status  # получи статус загрузки данных (True / False)
+
