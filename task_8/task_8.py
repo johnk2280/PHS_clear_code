503b096b169dc30a1b8e89464ef8ec8df476e275
johnk2280 <johnk2280@gmail.com>
2021-08-09 21:22:17 +0500

    [add] added task_8

---
+++ task_8/task_8.py
@@ -0,0 +1,16 @@
+#  Т.к. на курсах ВШ Серге бобровского большое внимание уделяется именованию переменных,
+# не смог найти в своем коде имена констант для редактирования.
+# Ниже привел имена, использовал ранее.
+
+STATIC_URL
+MEDIA_URL
+MEDIA_ROOT
+AUTH_USER_MODEL
+LANGUAGE_CODE
+TIME_ZONE
+SECRET_KEY
+BASE_DIR
+DATABASES
+CAR_DATA
+CARS
+BRANDS
